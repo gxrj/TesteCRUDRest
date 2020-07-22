fc23003a7204b3d110086c03b21a437f205993e6
gxrj <gsouzafone@gmail.com>
2020-07-22 18:47:39 -0300

    Implementado o mêtodo POST de arquivo em ArquivoService.java; Porém o envio de arquivos ainda não se encontra funcional

--- src/main/java/api/App.java
+++ src/main/java/api/App.java
@@ -1,5 +1,7 @@
 package api;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
@@ -15,6 +17,13 @@ public class App extends Application {
         Set<Class<?>> resources = new java.util.HashSet<>();
         addRestResourceClasses(resources);
         return resources;
+    }
+    /*O metodo getProperties() registra o multiparfeature no servlet*/
+    public Map<String, Object> getProperties() {
+        final Map<String, Object> properties = new HashMap<>();
+        properties.put("jersey.config.server.provider.classnames",
+                       "org.glassfish.jersey.media.multipart.MultiPartFeature");
+        return properties;
     }
 
     private void addRestResourceClasses(Set<Class<?>> resources) {
--- src/main/java/controller/ArquivoService.java
+++ src/main/java/controller/ArquivoService.java
@@ -1,24 +1,85 @@
 package controller;
 
+import dao.ArquivoDao;
+import dao.UsuarioDao;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import model.Arquivo;
+import model.Usuario;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 /**
  *
  * @author Souza
  */
-
 @Path("/docs")
 public class ArquivoService {
-    
+
     @Context
     private UriInfo context;
+    @EJB
+    ArquivoDao arquivoDao;
+    @EJB
+    UsuarioDao usuarioDao;
+
+    @Path("/{email}")
+    public List<Arquivo> getArquivos(@QueryParam("email") String email) {
+        try {
+            return arquivoDao.getArquivos(email);
+        } catch (Exception e) {
+            return null;
+        }
+    }
     
-    
-    public Response upload(){
+    @POST
+    @Path("/{email}/upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response upload(
+            @FormDataParam("file") InputStream in,
+            @FormDataParam("file") FormDataContentDisposition metadados,
+            @PathParam("email")String email) throws Exception{
         
+            
+            Usuario u = usuarioDao.getUsuario(email);
+            Arquivo arq = new Arquivo();
+            arq.setNome(metadados.getFileName());
+            arq.setUsuario(u);
+            
+            
+            arq.setConteudo(this.parse(in, metadados));
+            
+            u.getArquivos().add(arq);
+           
+            arquivoDao.add(arq);
+            usuarioDao.update(u);
+            
         return Response.status(200).entity("Arquivo enviado com sucesso").build();
+    }
+    
+    public byte[] parse(InputStream in, FormDataContentDisposition meta) throws FileNotFoundException{
+        
+        byte[] blob = new byte[(int) meta.getSize()];
+       
+        try{
+            in.read(blob);
+            in.close();
+        }catch(IOException ex){
+            ex.printStackTrace();
+            return null;
+        }
+        return blob;
     }
 }
